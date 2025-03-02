13326bba690e5a434f16260bce44e0b0d32e8217
TheGreatDamBuilder <boomgamerlol3389xd@gmail.com>
2025-03-02 22:56:46 +0700

    Add files via upload

---
+++ erer3.py
@@ -0,0 +1,30 @@
+print()
+base = float(input("รับค่าฐาน: "))
+height = float(input("รับค่าความสูง: "))
+triangle_area = 0.5 * base * height
+print("เเสดงพื้นที่สามเหลี่ยม")
+print(f"{triangle_area:.6f}")
+print()
+
+print()
+C = float(input("กรอกค่าอุณหภูมิ (องศาเซลเซียส): "))
+F = (9/5) * C + 32
+K = C + 273.15
+print(f"อุณหภูมิในหน่วยฟาเรนไฮต์: {F:.2f} °F")
+print(f"อุณหภูมิในหน่วยเคลวิน: {K:.2f} K")
+print()
+print("a = {10, 20, 30, 40, 50,60}\nb = {30, 40, 50, 60, 70,80}\nนักเรียนเขียนคำสั่งด้วยภาษาไบทอนหาเซตของที่กำหนดให้แสดงผลตามที่กำหนด {70, 40, 10, 80, 50, 20, 60, 30} {40, 50, 60, 30} {10, 20} {80, 20, 70, 10}")
+
+a = {10, 20, 30, 40, 50, 60}
+b = {30, 40, 50, 60, 70, 80}
+result1 = a.union(b)
+result2 = a.intersection(b)
+result3 = a.difference(b)
+result4 = a.symmetric_difference(b)
+print()
+import time
+time.sleep(.5)
+print(result1)
+print(result2)  
+print(result3)  
+print(result4)  
